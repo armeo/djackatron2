02899ab2e8b66b84f67609c91893eb096d96d36b
pronto.arm <arm@prontomarketing.com>
2013-07-13 16:25:55 +0700

    add time service test

---
+++ src/test/java/com/test/djackatron2/service/TimeServiceTest.java
@@ -0,0 +1,12 @@
+package com.test.djackatron2.service;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+
+public class TimeServiceTest {
+
+	@Test
+	public void 
+}
